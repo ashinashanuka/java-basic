01f3be72b930a4bb9e3de3c8425fd208073f8d9c
Ashina Shanuka <45147260+ashinashanuka@users.noreply.github.com>
2019-09-10 14:10:57 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String name ="David";
+        int age = 42;
+        double score =15.9;
+        char group = 'Z';
+    }
+}
