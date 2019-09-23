278547fca1fc81f8470c766ef348579581a22415
Ashina Shanuka <45147260+ashinashanuka@users.noreply.github.com>
2019-09-23 09:57:30 +0530

    Add files via upload

---
+++ JavaApplication3.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author ashin
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int age = 30;
+
+if (age < 16) {
+   System.out.println("Too Young");
+} else { 
+   System.out.println("Welcome!");
+}
+    }
+    
+}
